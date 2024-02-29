d1c42285276004c8e9b0d1946f6339458973a434
natthawatthap <natthawatthap@gmail.com>
2024-03-01 03:25:01 +0700

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Your App Name',
+        short_name: 'App',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'public/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'public/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
